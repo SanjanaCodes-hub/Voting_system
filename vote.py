d93d9033f9e6ed222bca8100a113c66e4cc9ec3e
SanjanaCodes-hub <sanjanadavagi@gmail.com>
2026-09-12 09:28:07 +0300

    Update vote.py

--- vote.py
+++ vote.py
@@ -1,5 +1,7 @@
 age = int(input("Enter your Age: "))
 if(age < 18 ):
   print("You are ineligible to vote")
+  print("Please wait untill you turn 18.")
 else:
   print("You are eligible to vote.")
+  print("Free to do so.")
